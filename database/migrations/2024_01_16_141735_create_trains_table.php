b3327ce8db01fdc76d65c96d84040abe9392ae25
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-01-16 15:29:39 +0100

    add: create trains table migration

---
+++ database/migrations/2024_01_16_141735_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('stazione_di_partenza');
+            $table->string('stazione_di_arrivo');
+            $table->dateTime('orario_partenza');
+            $table->dateTime('orario_arrivo');
+            $table->string('codice_treno');
+            $table->tinyInteger('n_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
